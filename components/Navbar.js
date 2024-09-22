eb4d6386c50631c02bbfed2b5794e1d6762f31ad
Aleks-Nazarenko <nazarenko@gmx.de>
2024-09-22 17:51:56 +0200

    No Message

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,31 @@
 // components/Navbar.js
 import Link from 'next/link';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Dropdown from 'react-bootstrap/Dropdown';
 
-export default function Navbar() {
+export default function NavbarNazarenko() {
     return (
-        <nav>
-            <ul>
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/about">About</Link></li>
-                <li><Link href="/contact">Contact</Link></li>
-            </ul>
+        <Navbar expand="lg" className="an-navbar">
+            <Container fluid>
+                <Navbar.Brand href="/">Navbar</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbar" />
+                <Navbar.Collapse id="navbar">
+                    <Nav className="me-auto">
+                        <Link href="/" passHref legacyBehavior>
+                            <a className="nav-link">Home</a>
+                        </Link>
+                        <Link href="/about" passHref legacyBehavior>
+                            <a className="nav-link">Home</a>
+                        </Link>
+                        <Link href="/contact" passHref legacyBehavior>
+                            <a className="nav-link">Home</a>
+                        </Link>
+                    </Nav>
+                </Navbar.Collapse>
 
-        </nav>
+            </Container>
+        </Navbar>
     );
 }
