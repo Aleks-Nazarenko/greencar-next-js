35d6859a880dfd6a6f368b41c12d839665451e67
Aleks-Nazarenko <nazarenko@gmx.de>
2024-10-29 18:15:06 +0100

    No Message

--- pages/pkw-partikelfilter/pkw-nachruestfilter/[slug].js
+++ pages/pkw-partikelfilter/pkw-nachruestfilter/[slug].js
@@ -5,6 +5,7 @@ import styles from "@/styles/Home.module.css";
 import axios from 'axios';
 import Link from "next/link";
 import { useRouter } from 'next/router';
+import {convertRelativeUrls} from "@/utils/convertRelativeUrls";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,28 +21,54 @@ export async function getStaticPaths() {
     return { paths, fallback: false };
 }
 export async function getStaticProps({ params }) {
+    // Base URL of your Joomla server (adjust this to your Joomla installation URL)
+    const joomlaBaseUrl = 'https://joomla.nazarenko.de';
     // Fetch data from Joomla API
     const res = await axios.get(`https://joomla.nazarenko.de/index.php?option=com_nazarenkoapi&view=product&code=${params.slug}&format=json`);
     const product = res.data;
    // console.log(product);
+    // Fetch data for the footer from Joomla API
+    const resFooter = await fetch('https://joomla.nazarenko.de/index.php?option=com_nazarenkoapi&task=articleWithModules&id=4&format=json');
+    const footerData = await resFooter.json();
+    // Extract the footer article from the response
+    const footerArticle = footerData.article || null;
 
+    // Convert relative URLs in the footer content to absolute URLs
+    if (footerArticle && footerArticle.introtext) {
+        footerArticle.introtext = convertRelativeUrls(footerArticle.introtext, joomlaBaseUrl);
+    }
     // Pass data to the page via props
-    return { props: { product } };
+    return { props: { product, footerArticle } };
 }
 
 
-function Product({ product }) {
+function Product({ product, footerArticle }) {
     const router = useRouter();
 
     if (router.isFallback) {
-        return <div>Loading...</div>;
+        return (
+            <div className="row g-0 justify-content-center">
+               <div className="col-auto">Loading...</div>
+            </div>
+        );
     }
-
     return (
-        <div>
-            <h1>{product.product_name}</h1>
-
-        </div>
+        <>
+            <main>
+                <div className="container-fluid container-greencar">
+                    <div className="row g-0 p-4">
+                        <h1>{product.product_name}</h1>
+                    </div>
+                </div>
+            </main>
+            <footer>
+                <div className="container-fluid container-footer container-greencar">
+                    <div className="row g-0 p-4">
+                        <div dangerouslySetInnerHTML={{ __html: footerArticle.introtext}} />
+                    </div>
+                </div>
+            </footer>
+        </>
     );
 }
 
