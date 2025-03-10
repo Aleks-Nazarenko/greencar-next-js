055b4c577d461682eb8ee25edf33d170b28496c4
Aleks-Nazarenko <nazarenko@gmx.de>
2025-03-10 18:05:09 +0100

    No Message

--- pages/404.js
+++ pages/404.js
@@ -1,14 +1,10 @@
-import {convertRelativeUrls} from "@/utils/convertRelativeUrls";
-import {JOOMLA_API_BASE} from "@/utils/config";
-import {JOOMLA_URL_BASE} from "@/utils/config";
-
 
 export default function Custom404() {
     return (
         <>
 
                     <div className="row g-0 p-4 justify-content-center">
-                        <h1 className="col-auto">404 - Page Not Found</h1>
+                        <h1 className="col-auto">404 - Seite nicht gefunden</h1>
                     </div>
 
         </>
